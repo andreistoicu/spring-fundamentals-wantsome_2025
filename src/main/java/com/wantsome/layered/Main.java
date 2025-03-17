63c8cf93ea8184a2938689afc3a5ff3ee96800f7
andrei <andrei_stoicu@yahoo.com>
2025-03-17 23:04:41 +0200

    package change

--- src/main/java/com/wantsome/layered/Main.java
+++ src/main/java/com/wantsome/layered/Main.java
@@ -1,4 +1,4 @@
-package com.wantsome;
+package com.wantsome.layered;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/wantsome/layered/web_controllers/UserController.java
+++ src/main/java/com/wantsome/layered/web_controllers/UserController.java
@@ -1,4 +1,4 @@
-package com.wantsome.layered.web_controllers.controller;
+package com.wantsome.layered.web_controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
