c1208386de1a843503481273294e6cd35a1e0e1a
andrei <andrei_stoicu@yahoo.com>
2025-03-17 20:47:49 +0200

    get & post example

---
+++ src/main/java/wantsome/layered/web_controllers/controller/BeanController.java
@@ -0,0 +1,23 @@
+package wantsome.layered.web_controllers.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class BeanController {
+
+    //@RequestMapping(method = RequestMethod.GET, value ="/users" ) //-> metoda utilizata in paralel, echivalent cu metoda de jos
+    @GetMapping("/users")
+    public String getUsers() {
+        return "users";
+    }
+
+    //@RequestMapping(method = RequestMethod.POST, value= "/submitForm")
+    @PostMapping("/submitForm")
+    public String submitForm(@RequestParam("name") String name , @RequestParam("email") String email) {
+        System.out.println("Name: " + name);
+        System.out.println("Email:" + email);
+        return "success";
+    }
+
+}
