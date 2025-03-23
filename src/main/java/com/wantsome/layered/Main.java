19848eb022dd41f00914e13d8fe2404dc473090a
andrei <andrei_stoicu@yahoo.com>
2025-03-23 23:15:17 +0200

    remove 'exclude' from Main class

--- src/main/java/com/wantsome/layered/Main.java
+++ src/main/java/com/wantsome/layered/Main.java
@@ -2,12 +2,8 @@ package com.wantsome.layered;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-//in cazul in care nu folosim o baza de date, atunci adaugam optiunea de 'exclude'
-// intrucat SpringBoot scaneaza la runtime o baza de date si daca nu o gaseste atunci arunca eroare
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello All");
