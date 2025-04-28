d3e3514c0c2b88940b900e36cde0e5a828482182
andrei <andrei_stoicu@yahoo.com>
2025-04-28 18:30:57 +0300

    exercise

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(Main.class, args);
+    }
+}
