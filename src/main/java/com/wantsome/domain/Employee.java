0b5bb5580f5ebaf02651fe686113d7cc0a2be08b
andrei <andrei_stoicu@yahoo.com>
2025-04-28 20:45:44 +0300

    fix table name for entities

--- src/main/java/com/wantsome/domain/Employee.java
+++ src/main/java/com/wantsome/domain/Employee.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import java.util.Objects;
 
 @Entity
+@Table(name = "employees")
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
